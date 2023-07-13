a0c4a2f7bb2ccbaca638b3a84106a7165df2ba40
Bai Yunpeng <e0917623@u.nus.edu>
2023-07-13 13:35:46 +0800

    Test - Still does not stop early.

--- huc/envs/mobile_reading/MDPRead.py
+++ huc/envs/mobile_reading/MDPRead.py
@@ -311,11 +311,11 @@ class MDPRead(Env):
                 # Time out
                 reward += -10
 
-        # # TODO debug delete later when training
-        # print(f"The step is: {self._steps}, \n"
-        #       f"The action is: {action[0]}, the deployed target is: {self._deployed_attention_target_mjidx}, "
-        #       f"the on target steps is: {self._on_target_steps}, \n"
-        #       f"the reading memory is: {self._mental_state['reading_memory']}, \n"
-        #       f"The reward is: {reward}, \n")
+        # TODO debug delete later when training
+        print(f"The step is: {self._steps}, \n"
+              f"The action is: {action[0]}, the deployed target is: {self._deployed_attention_target_mjidx}, "
+              f"the on target steps is: {self._on_target_steps}, \n"
+              f"the reading memory is: {self._mental_state['reading_memory']}, \n"
+              f"The reward is: {reward}, \n")
 
         return self._get_obs(), reward, terminate, info
